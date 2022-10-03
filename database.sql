7406a47fa67346dd7193378266b90ccddc9d0122
Blake Smith <blake.smith.mn@gmail.com>
2022-10-03 11:35:49 -0500

    fixed SQL errors

--- database.sql
+++ database.sql
@@ -1,3 +1,9 @@
+
+DROP TABLE "movies"; 
+
+DROP TABLE "genres";
+
+DROP TABLE "movies_genres";
 -- CREATE DATABASE "saga_movies_weekend"
 
 
@@ -30,12 +36,12 @@ CREATE TABLE "movies_genres" (
 INSERT INTO "movies" ("title", "poster", "description")
 VALUES 
 ('Everything Everywhere All at Once', 'https://image.tmdb.org/t/p/original//w3LxiVYdWWRvEVdn5RYq6jIqkb1.jpg', 'An aging Chinese immigrant is swept up in an insane adventure, where she alone can save what''s important to her by connecting with the lives she could have led in other universes.'),
-('Her', 'https://image.tmdb.org/t/p/original//yk4J4aewWYNiBhD49WD7UaBBn37.jpg', 'In the not so distant future, Theodore, a lonely writer purchases a newly developed operating system designed to meet the user's every needs. To Theodore's surprise, a romantic relationship develops between him and his operating system. This unconventional love story blends science fiction and romance in a sweet tale that explores the nature of love and the ways that technology isolates and connects us all.'),
+('Her', 'https://image.tmdb.org/t/p/original//yk4J4aewWYNiBhD49WD7UaBBn37.jpg', 'In the not so distant future, Theodore, a lonely writer purchases a newly developed operating system designed to meet the user''s every needs. To Theodore''s surprise, a romantic relationship develops between him and his operating system. This unconventional love story blends science fiction and romance in a sweet tale that explores the nature of love and the ways that technology isolates and connects us all.'),
 ('Nope', 'https://image.tmdb.org/t/p/original//AcKVlWaNVVVFQwro3nLXqPljcYA.jpg', 'Residents in a lonely gulch of inland California bear witness to an uncanny, chilling discovery.'),
 ('Spirited Away', 'https://image.tmdb.org/t/p/original//39wmItIWsg5sZMyRUHLkWBcuVCM.jpg', 'A young girl, Chihiro, becomes trapped in a strange new world of spirits. When her parents undergo a mysterious transformation, she must call upon the courage she never knew she had to free her family.'),
 ('Lady Bird', 'https://image.tmdb.org/t/p/original//iySFtKLrWvVzXzlFj7x1zalxi5G.jpg', 'Lady Bird McPherson, a strong willed, deeply opinionated, artistic 17 year old comes of age in Sacramento. Her relationship with her mother and her upbringing are questioned and tested as she plans to head off to college.'),
 ('Inception', 'https://image.tmdb.org/t/p/original//8IB2e4r4oVhHnANbnm7O3Tj6tF8.jpg', 'Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets is offered a chance to regain his old life as payment for a task considered to be impossible: "inception", the implantation of another person''s idea into a target''s subconscious.'),
-('Get Out', 'https://image.tmdb.org/t/p/original//tFXcEccSQMf3lfhfXKSU9iRBpa3.jpg', 'Chris and his girlfriend Rose go upstate to visit her parents for the weekend. At first, Chris reads the family's overly accommodating behavior as nervous attempts to deal with their daughter's interracial relationship, but as the weekend progresses, a series of increasingly disturbing discoveries lead him to a truth that he never could have imagined.'),
+('Get Out', 'https://image.tmdb.org/t/p/original//tFXcEccSQMf3lfhfXKSU9iRBpa3.jpg', 'Chris and his girlfriend Rose go upstate to visit her parents for the weekend. At first, Chris reads the family''s overly accommodating behavior as nervous attempts to deal with their daughter''s interracial relationship, but as the weekend progresses, a series of increasingly disturbing discoveries lead him to a truth that he never could have imagined.'),
 ('Parasite', 'https://image.tmdb.org/t/p/original//7IiTTgloJzvGI1TAYymCfbfl3vT.jpg', 'All unemployed, Ki-taek''s family takes peculiar interest in the wealthy and glamorous Parks for their livelihood until they get entangled in an unexpected incident.' ),
 ('The Shining', 'https://image.tmdb.org/t/p/original//nRj5511mZdTl4saWEPoj9QroTIu.jpg', 'Jack Torrance accepts a caretaker job at the Overlook Hotel, where he, along with his wife Wendy and their son Danny, must live isolated from the rest of the world for the winter. But they aren''t prepared for the madness that lurks within.');
 
@@ -54,7 +60,7 @@ VALUES
 ('Science Fiction'),  --9
 ('LGBTQIA+'), --10
 ('Thriller'),      --11
-('Unspecified') --12;        
+('Unspecified'); --12;        
 
 
 -- starter movies and genres data
